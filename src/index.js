9c924cd866accb19f3fc51e74db8d414e3ab1401
Andrew David Murray <andrewmurraydavid@gmail.com>
2019-04-02 11:51:21 +0300

    Created basic project structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var faker = require('faker');
+
+
+/**
+ * @todo Go through obj keys recursively
+ * @body We need to go through the object keys recursively to ensure performance and optinization are kept
+ * @param Object obj 
+ */
+const generate = obj => {
+
+}
+
+
+console.log(faker.fake("name: {{name.suffix}}"));
